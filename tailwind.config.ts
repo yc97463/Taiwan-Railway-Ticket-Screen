7b03e53657e4dec72c563b26b3ed8cebf0627d2c
yc97463 <hi@imych.one>
2024-10-14 01:11:30 +0800

    feat: init next.js

---
+++ tailwind.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        tr: {
+          blue: "#1285BB",
+          orange: "#FA6203",
+          yellow: "#FFC600",
+        }
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
